d3931306f2bf99b3bbb0ee42f2874bf25c50fa1d
Sirfreud <mikevaldez92@gmail.com>
2017-09-21 00:38:45 -0500

    Created ChatsController and wrote fetchMessages() and sendMessage

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'ChatsController@index');
+Route::get('messages', 'ChatsController@fetchMessage');
+Route::post('messages', 'ChatsController@sendMessage');
 

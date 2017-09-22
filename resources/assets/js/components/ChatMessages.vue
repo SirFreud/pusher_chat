7666ec781ef821384de3c513ccb0145c481cf085
Sirfreud <mikevaldez92@gmail.com>
2017-09-22 00:07:14 -0500

    built out ChatMessages and Chatform views

---
+++ resources/assets/js/components/ChatMessages.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="chat">
+        <li v-for="message in messages" class="left clearfix">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">
+                        {{ message.user.name }}
+                    </strong>
+                </div>
+                <p>
+                    {{ message.message }}
+                </p>
+            </div>
+        </li>
+            
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: ['messages']
+    };
+</script>
